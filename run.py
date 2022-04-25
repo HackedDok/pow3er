dd64580712a9b4db8c731671b38140163fc08fcb
Hedi Gader <94740559+HackedDok@users.noreply.github.com>
2022-04-25 18:26:01 +0200

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Hedi").teaching()
-elif 'aarch' in arc:
-	__import__("Hedi64").teaching()
-else:
-	exit(f' Unknow device machine {arc}')
